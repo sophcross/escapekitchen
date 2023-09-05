327c257fe7e08c26e18c50384a052f3eed7c3ff8
Unaidedbruh <143032126+Unaidedbruh@users.noreply.github.com>
2023-09-05 18:37:12 -0500

    Changed Colors & Created Scripts

---
+++ Escape Kitchen/Assets/Scripts/Apple.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ingredient : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SelectIngredient()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+
+        }
+    }
+}
